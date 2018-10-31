eb35442eee1416efa3e669f44f03efbe24317e2c
chun-sung <springstar@daum.net>
2018-10-31 11:13:58 +0900

    v 0.1.6 javascript apply quickmenu

---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+var abc = document.querySelector('.abc')
+ abc.addEventListener('click', function(){
+    alert('아흑~!');
+ });
+
+var question = document.querySelector('.id')
+question.addEventListener('click', function () {
+    var e = prompt('ID 입력', 'ID');    
+    if (e == 'ID') {
+        alert('회원 가입을 해주세요');
+    } else if(e) {
+        alert('당신의 ID 는' + e + '입니다.');
+    } else {
+        alert('ID가 없다구요?');
+    }
+});
+var question = document.querySelector('.question')
+    question.addEventListener('click', function(){
+        var e = confirm('궁금하신거 있으세용?','질문 하세요');   
+        if(e){
+            alert('think Big');
+        } else {
+            alert('없다구요?');
+        }
+  });
+
+var hidden = document.querySelector('.hidden')
+    hidden.addEventListener('click', function(){
+        hidden.style.visibility="hidden";
+});
+
+var change = document.querySelector('.change')
+    change.addEventListener('click', function(){
+        change.innerHTML = "변경됨";
+    });
